45356883d8eac7d7ba8e75af43d35a89020b3c84
Joy Kumer Tarafder <joykumer698@gmail.com>
2026-08-29 02:25:26 +0600

    Update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,7 +14,19 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Register the override BEFORE evaluation happens
+    if (project.name != "app") {
+        afterEvaluate {
+            if (project.hasProperty("android")) {
+                project.extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+                    compileSdkVersion(36)
+                }
+            }
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }

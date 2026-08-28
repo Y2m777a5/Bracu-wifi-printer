8c127c19a7fb86283eebcea4e0e6095a2c5e0dbd
Joy Kumer Tarafder <joykumer698@gmail.com>
2026-08-28 23:16:04 +0600

    Update print history page to reflect queued status

--- lib/views/print_history_page.dart
+++ lib/views/print_history_page.dart
@@ -14,8 +14,8 @@ class PrintHistoryPage extends StatelessWidget {
             child: ListTile(
               leading: Icon(Icons.picture_as_pdf, color: Colors.red),
               title: Text('Assignment_02.pdf'),
-              subtitle: Text('Sent to 10.10.0.50 • 2 pages'),
-              trailing: Text('Success', style: TextStyle(color: Colors.green)),
+              subtitle: Text('Sent to 10.10.0.50'),
+              trailing: Text('Queued', style: TextStyle(color: Colors.green)),
             ),
           ),
         ],

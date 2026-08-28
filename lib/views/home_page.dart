c81bbd75b598b1372f03ba97e07655461ae455e8
Joy Kumer Tarafder <joykumer698@gmail.com>
2026-08-28 22:59:10 +0600

    Create home_page.dart

---
+++ lib/views/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import '../services/lpd_printer_service.dart';
+import '../services/wifi_service.dart';
+
+class CampusPrinterHomePage extends StatefulWidget {
+  const CampusPrinterHomePage({super.key});
+
+  @override
+  State<CampusPrinterHomePage> createState() => _CampusPrinterHomePageState();
+}
+
+class _CampusPrinterHomePageState extends State<CampusPrinterHomePage> {
+  // Your UI controller logic, _handleWifiLogin, _handlePrintSubmission, etc.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ... your existing UI layout
+    );
+  }
+}
